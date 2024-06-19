c2d2e4664113adc13f09d14cf60461e7079c5b99
Kvotheo <vitorjoao.cl@gmail.com>
2024-06-18 21:33:54 -0300

    Inicio da camada application

---
+++ CleanArchMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entitites;
+
+namespace CleanArchMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            //Realizando o mapeamento com Category da entities com o dto da application
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
