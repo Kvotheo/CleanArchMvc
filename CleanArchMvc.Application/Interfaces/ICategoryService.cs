c1086d5012d7c51efcb8cdff28304b56428b2f17
Kvotheo <vitorjoao.cl@gmail.com>
2024-06-20 16:42:01 -0300

    Ajustes  na application

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+
+        /// <summary>
+        /// Retornando as categorias
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+
+        /// <summary>
+        /// Pega a categoria pelo ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<CategoryDTO> GetCategoryById(int? id);
+        
+        /// <summary>
+        /// Adiciona uma nova categoria
+        /// </summary>
+        /// <param name="categoryDTO"></param>
+        /// <returns></returns>
+        Task Add(CategoryDTO categoryDTO);
+
+        /// <summary>
+        /// Realiza a alteração da categoria
+        /// </summary>
+        /// <param name="categoryDTO"></param>
+        /// <returns></returns>
+        Task Update(CategoryDTO categoryDTO);
+
+        /// <summary>
+        /// Remove a categoria
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Remove(int? id);
+
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Migrations/20240616035207_SeedProducts.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products (Name, Description, Price, Stock, Image, CategoryId) " +
+                                  "VALUES ('Caderno', 'Caderno espiral 100 folhas', 7.45, 50, 'cardeno1.jpg', 1);");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
